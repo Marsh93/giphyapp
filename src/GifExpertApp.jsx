975f1601bc8dd4333dd2779378b208c03cb7a6a9
Omar Zavala <alexoamr1993@gmail.com>
2022-02-25 02:00:01 -0500

    test pre-commit passed successfully

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -11,8 +11,6 @@ import "animate.css";
  * ? Should parameters
  */
 
-const user = "User de prueba";
-
 export default function GifExpertApp() {
   const [categories, setCategories] = useState([]);
   const [hasError, setHasError] = useState(false);
